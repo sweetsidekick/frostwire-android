5b207ba436b96084721ab047fa015c2c5ca2f9f2
gubatron <gubatron@gmail.com>
2012-03-29 15:38:00 -0400

    removed warning

--- src/gui/com/frostwire/android/gui/views/AbstractSwipeDetector.java
+++ src/gui/com/frostwire/android/gui/views/AbstractSwipeDetector.java
@@ -7,6 +7,7 @@ import android.view.View.OnTouchListener;
 
 public abstract class AbstractSwipeDetector extends SimpleOnGestureListener implements OnTouchListener {
     
+    @SuppressWarnings("unused")
     private static final String TAG = "FW.AbstractSwipeDetector";
     
     private final int MIN_DISTANCE = 100;
