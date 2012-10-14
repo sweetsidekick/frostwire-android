85557282368fd6e31d01d6f30c260bd9866f4d54
gubatron <gubatron@gmail.com>
2012-10-14 12:25:28 -0400

    Player notifier now shows if the current song is paused. Version bump for tester build.

--- src/gui/com/frostwire/android/gui/views/PlayerNotifierView.java
+++ src/gui/com/frostwire/android/gui/views/PlayerNotifierView.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.frostwire.android.R;
 import com.frostwire.android.core.FileDescriptor;
+import com.frostwire.android.core.player.CoreMediaPlayer;
 import com.frostwire.android.gui.activities.MediaPlayerActivity;
 import com.frostwire.android.gui.services.Engine;
 
@@ -54,12 +55,31 @@ public class PlayerNotifierView extends LinearLayout implements Refreshable {
         View.inflate(getContext(), R.layout.view_player_notifier, this);
         statusText = (TextView) findViewById(R.id.view_player_notifier_status);
         statusContainer = (LinearLayout) findViewById(R.id.view_player_notifier_status_container);
+        
+        refreshPlayerStateIndicator();
+    }
+
+    private void refreshPlayerStateIndicator() {
+        CoreMediaPlayer mediaPlayer = Engine.instance().getMediaPlayer();
+        if (mediaPlayer == null) {
+            return;
+        }
+
+        TextView isPlayingText = (TextView) findViewById(R.id.view_player_notifier_playing);
+        
+        if (!mediaPlayer.isPlaying()) {
+            isPlayingText.setText(R.string.paused);
+        } else {
+            isPlayingText.setText(R.string.playing);
+        }
     }
 
     @Override
     public void refresh() {
         FileDescriptor fd = Engine.instance().getMediaPlayer().getCurrentFD();
         String status = "";
+        
+        refreshPlayerStateIndicator();
 
         if (fd != null) {
             status = fd.artist + " - " + fd.title;
