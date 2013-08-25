f92bc50fcbfcf2a56cabce645da49ff5a24bfeed
gubatron <gubatron@gmail.com>
2013-08-25 04:13:48 -0400

    cleanup

--- src/gui/com/frostwire/android/gui/activities/MainActivity.java
+++ src/gui/com/frostwire/android/gui/activities/MainActivity.java
@@ -33,7 +33,6 @@ import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
