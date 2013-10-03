5d3f6ca74d1961d23a7cb1b73aa182441fc969af
Alden Torres <aldenml@gmail.com>
2013-10-03 11:26:29 -0400

    Reading ux stats setting from remote message.

--- src/gui/com/frostwire/android/gui/SoftwareUpdater.java
+++ src/gui/com/frostwire/android/gui/SoftwareUpdater.java
@@ -41,11 +41,14 @@ import com.frostwire.android.core.ConfigurationManager;
 import com.frostwire.android.core.Constants;
 import com.frostwire.android.core.HttpFetcher;
 import com.frostwire.android.gui.services.Engine;
+import com.frostwire.android.gui.util.OSUtils;
 import com.frostwire.android.gui.util.SystemUtils;
 import com.frostwire.android.gui.util.UIUtils;
 import com.frostwire.android.util.ByteUtils;
 import com.frostwire.android.util.StringUtils;
 import com.frostwire.util.JsonUtils;
+import com.frostwire.uxstats.UXStats;
+import com.frostwire.uxstats.UXStatsConf;
 
 /**
  * 
@@ -317,6 +320,18 @@ public final class SoftwareUpdater {
         //ConfigurationManager.instance().setBoolean(Constants.PREF_KEY_GUI_INITIALIZE_OFFERCAST, update.config.offercast);
         ConfigurationManager.instance().setBoolean(Constants.PREF_KEY_GUI_SHOW_FREE_APPS_MENU_ITEM, update.config.freeApps);
         ConfigurationManager.instance().setBoolean(Constants.PREF_KEY_GUI_INITIALIZE_APPIA, update.config.appia);
+
+        if (update.config.uxEnabled && ConfigurationManager.instance().getBoolean(Constants.PREF_KEY_UXSTATS_ENABLED)) {
+            String os = OSUtils.getOSVersionString();
+            String fwversion = Constants.FROSTWIRE_VERSION_STRING;
+            String fwbuild = Constants.FROSTWIRE_BUILD;
+            int period = update.config.uxPeriod;
+            int minEntries = update.config.uxMinEntries;
+            int maxEntries = update.config.uxMaxEntries;
+
+            UXStatsConf context = new UXStatsConf(os, fwversion, fwbuild, period, minEntries, maxEntries);
+            UXStats.instance().setContext(context);
+        }
     }
 
     private void notifyConfigurationUpdateListeners() {
@@ -359,6 +374,11 @@ public final class SoftwareUpdater {
         //public boolean offercast = true;
         public boolean freeApps = true;
 
+        // ux stats
+        public boolean uxEnabled = false;
+        public int uxPeriod = 3600;
+        public int uxMinEntries = 10;
+        public int uxMaxEntries = 10000;
     }
 
     public void removeConfigurationUpdateListener(Object slideMenuFragment) {
