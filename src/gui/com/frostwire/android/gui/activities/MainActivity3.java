bef34141a39ef2693e8d22cd2dae497b2c0cf3c6
Alden Torres <aldenml@gmail.com>
2013-10-10 15:23:43 -0400

    Click still working.

--- src/gui/com/frostwire/android/gui/activities/MainActivity3.java
+++ src/gui/com/frostwire/android/gui/activities/MainActivity3.java
@@ -1,5 +1,6 @@
 package com.frostwire.android.gui.activities;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,17 +9,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmlpull.v1.XmlPullParser;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.content.res.Configuration;
 import android.content.res.XmlResourceParser;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -27,6 +35,10 @@ import android.widget.TextView;
 import com.frostwire.android.R;
 import com.frostwire.android.core.ConfigurationManager;
 import com.frostwire.android.core.Constants;
+import com.frostwire.android.core.DesktopUploadRequest;
+import com.frostwire.android.core.DesktopUploadRequestStatus;
+import com.frostwire.android.core.FileDescriptor;
+import com.frostwire.android.gui.Librarian;
 import com.frostwire.android.gui.PeerManager;
 import com.frostwire.android.gui.fragments.AboutFragment;
 import com.frostwire.android.gui.fragments.BrowsePeerFragment;
@@ -35,9 +47,16 @@ import com.frostwire.android.gui.fragments.BrowsePeersFragment;
 import com.frostwire.android.gui.fragments.MainFragment;
 import com.frostwire.android.gui.fragments.SearchFragment;
 import com.frostwire.android.gui.fragments.TransfersFragment;
+import com.frostwire.android.gui.services.DesktopUploadManager;
+import com.frostwire.android.gui.services.Engine;
+import com.frostwire.android.gui.transfers.TransferManager;
 import com.frostwire.android.gui.util.OSUtils;
+import com.frostwire.android.gui.util.UIUtils;
 import com.frostwire.android.gui.views.AbstractActivity;
 import com.frostwire.android.gui.views.AbstractListAdapter;
+import com.frostwire.android.gui.views.DesktopUploadRequestDialog;
+import com.frostwire.android.gui.views.DesktopUploadRequestDialogResult;
+import com.frostwire.android.gui.views.Refreshable;
 
 public class MainActivity3 extends AbstractActivity {
 
@@ -45,6 +64,10 @@ public class MainActivity3 extends AbstractActivity {
 
     private static final String FRAGMENT_STACK_TAG = "fragment_stack";
     private static final String CURRENT_FRAGMENT_KEY = "current_fragment";
+    private static final String DUR_TOKEN_KEY = "dur_token";
+    private static final String APPIA_STARTED_KEY = "appia_started";
+
+    private static boolean firstTime = true;
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -55,6 +78,12 @@ public class MainActivity3 extends AbstractActivity {
     private BrowsePeersFragment peers;
     private BrowsePeersDisabledFragment peersDisabled;
     private AboutFragment about;
+
+    // not sure about this variable, quick solution for now
+    private String durToken;
+
+    //private boolean offercastStarted = false;
+    private boolean appiaStarted = false;
 
     public MainActivity3() {
         super(R.layout.activity_main3, false, 2);
@@ -102,9 +131,281 @@ public class MainActivity3 extends AbstractActivity {
         //        };
         //        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        if (savedInstanceState == null) {
-            selectItem(0);
+        ImageButton buttonMenu = findView(R.id.activity_main_button_menu);
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleDrawer();
+            }
+        });
+
+        if (savedInstanceState != null) {
+            durToken = savedInstanceState.getString(DUR_TOKEN_KEY);
+            //offercastStarted = savedInstanceState.getBoolean(OFFERCAST_STARTED_KEY);
+            appiaStarted = savedInstanceState.getBoolean(APPIA_STARTED_KEY);
         }
+
+        addRefreshable((Refreshable) findView(R.id.activity_main_player_notifier));
+
+        onNewIntent(getIntent());
+    }
+    
+    private void showTransfers() {
+        if (!(getCurrentFragment() instanceof TransfersFragment)) {
+            switchFragment(R.id.menu_main_transfers);
+        }
+    }
+    
+    @Override
+    protected void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        onResumeFragments();
+
+        if (action != null && action.equals(Constants.ACTION_SHOW_TRANSFERS)) {
+            showTransfers();
+        } else if (action != null && action.equals(Constants.ACTION_OPEN_TORRENT_URL)) {
+            //Open a Torrent from a URL or from a local file :), say from Astro File Manager.
+            /**
+             * TODO: Ask @aldenml the best way to plug in NewTransferDialog.
+             * I've refactored this dialog so that it is forced (no matter if the setting
+             * to not show it again has been used) and when that happens the checkbox is hidden.
+             * 
+             * However that dialog requires some data about the download, data which is not
+             * obtained until we have instantiated the Torrent object.
+             * 
+             * I'm thinking that we can either:
+             * a) Pass a parameter to the transfer manager, but this would probably
+             * not be cool since the transfer manager (I think) should work independently from
+             * the UI thread.
+             * 
+             * b) Pass a "listener" to the transfer manager, once the transfer manager has the torrent
+             * it can notify us and wait for the user to decide wether or not to continue with the transfer
+             * 
+             * c) Forget about showing that dialog, and just start the download, the user can cancel it.
+             */
+
+            //Show me the transfer tab
+            Intent i = new Intent(this, MainActivity.class);
+            i.setAction(Constants.ACTION_SHOW_TRANSFERS);
+            i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(i);
+
+            //go!
+            TransferManager.instance().downloadTorrent(intent.getDataString());
+        } else if ((action != null && action.equals(Constants.ACTION_DESKTOP_UPLOAD_REQUEST)) || durToken != null) {
+            handleDesktopUploadRequest(intent);
+        }
+        // When another application wants to "Share" a file and has chosen FrostWire to do so.
+        // We make the file "Shared" so it's visible for other FrostWire devices on the local network.
+        else if (action != null && (action.equals(Intent.ACTION_SEND) || action.equals(Intent.ACTION_SEND_MULTIPLE))) {
+            handleSendAction(intent);
+        }
+
+        if (intent.hasExtra(Constants.EXTRA_DOWNLOAD_COMPLETE_NOTIFICATION)) {
+            showTransfers();
+            TransferManager.instance().clearDownloadsToReview();
+            try {
+                ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).cancel(Constants.NOTIFICATION_DOWNLOAD_TRANSFER_FINISHED);
+                Bundle extras = intent.getExtras();
+                if (extras.containsKey(Constants.EXTRA_DOWNLOAD_COMPLETE_PATH)) {
+                    File file = new File(extras.getString(Constants.EXTRA_DOWNLOAD_COMPLETE_PATH));
+                    if (file.isFile()) {
+                        UIUtils.openFile(this, file.getAbsoluteFile());
+                    }
+                }
+            } catch (Throwable e) {
+                LOG.warn("Error handling download complete notification", e);
+            }
+        }
+    }
+    
+    private void handleSendAction(Intent intent) {
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_SEND)) {
+            handleSendSingleFile(intent);
+        } else if (action.equals(Intent.ACTION_SEND_MULTIPLE)) {
+            handleSendMultipleFiles(intent);
+        }
+    }
+    
+    private void handleSendMultipleFiles(Intent intent) {
+        ArrayList<Uri> fileUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (fileUris != null) {
+            for (Uri uri : fileUris) {
+                shareFileByUri(uri);
+            }
+            UIUtils.showLongMessage(this, getString(R.string.n_files_shared, fileUris.size()));
+        }
+    }
+    
+    private void handleSendSingleFile(Intent intent) {
+        Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (uri == null) {
+            return;
+        }
+        shareFileByUri(uri);
+        UIUtils.showLongMessage(this, R.string.one_file_shared);
+    }
+    
+    private void shareFileByUri(Uri uri) {
+        if (uri == null) {
+            return;
+        }
+
+        FileDescriptor fileDescriptor = Librarian.instance().getFileDescriptor(uri);
+
+        if (fileDescriptor != null) {
+            fileDescriptor.shared = true;
+            Librarian.instance().updateSharedStates(fileDescriptor.fileType, Arrays.asList(fileDescriptor));
+        }
+    }
+    
+    private void handleDesktopUploadRequest(Intent intent) {
+        String action = intent.getAction();
+
+        if (durToken == null && action.equals(Constants.ACTION_DESKTOP_UPLOAD_REQUEST)) {
+            durToken = intent.getStringExtra(Constants.EXTRA_DESKTOP_UPLOAD_REQUEST_TOKEN);
+        }
+
+        final DesktopUploadManager dum = Engine.instance().getDesktopUploadManager();
+
+        if (dum == null) {
+            return;
+        }
+
+        DesktopUploadRequest dur = dum.getRequest(durToken);
+
+        if (durToken != null && dur != null && dur.status == DesktopUploadRequestStatus.PENDING) {
+            DesktopUploadRequestDialog dlg = new DesktopUploadRequestDialog(this, dur, new DesktopUploadRequestDialog.OnDesktopUploadListener() {
+                @Override
+                public void onResult(DesktopUploadRequestDialog dialog, DesktopUploadRequestDialogResult result) {
+                    switch (result) {
+                    case ACCEPT:
+                        dum.authorizeRequest(durToken);
+                        if (ConfigurationManager.instance().showTransfersOnDownloadStart()) {
+                            Intent i = new Intent(Constants.ACTION_SHOW_TRANSFERS);
+                            MainActivity3.this.startActivity(i.setClass(MainActivity3.this, MainActivity.class));
+                        }
+                        break;
+                    case REJECT:
+                        dum.rejectRequest(durToken);
+                        break;
+                    case BLOCK:
+                        dum.blockComputer(durToken);
+                        break;
+                    }
+                    durToken = null;
+                }
+            });
+
+            trackDialog(dlg).show();
+        }
+    }
+
+    private void toggleDrawer() {
+        if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+            mDrawerLayout.closeDrawer(mDrawerList);
+        } else {
+            mDrawerLayout.openDrawer(mDrawerList);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            super.onBackPressed();
+        } else {
+            handleLastBackPressed();
+        }
+
+        syncSlideMenu();
+        updateHeader(getCurrentFragment());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SEARCH) {
+            if (!(getCurrentFragment() instanceof SearchFragment)) {
+                switchFragment(R.id.menu_main_search);
+            }
+        } else if (keyCode == KeyEvent.KEYCODE_MENU) {
+            toggleDrawer();
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+
+        return true;
+    }
+    
+    public void switchFragment(int itemId) {
+        Fragment fragment = getFragmentByMenuId(itemId);
+        if (fragment != null) {
+            switchContent(fragment);
+        }
+    }
+    
+    private Fragment getFragmentByMenuId(int id) {
+        switch (id) {
+        case R.id.menu_main_search:
+            return search;
+        case R.id.menu_main_library:
+            return library;
+        case R.id.menu_main_transfers:
+            return transfers;
+        case R.id.menu_main_peers:
+            return getWifiSharingFragment();
+        case R.id.menu_main_about:
+            return about;
+        default:
+            return null;
+        }
+    }
+    
+    private Fragment getWifiSharingFragment() {
+        return (Fragment) (isWifiSharingEnabled() ? peers : peersDisabled);
+    }
+    
+    private boolean isWifiSharingEnabled() {
+        return Engine.instance().isStarted() && ConfigurationManager.instance().getBoolean(Constants.PREF_KEY_NETWORK_USE_UPNP);
+    }
+    
+    private void switchContent(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_content_frame, fragment, FRAGMENT_STACK_TAG).addToBackStack(null).commit();
+        getSupportFragmentManager().executePendingTransactions();
+        mDrawerLayout.openDrawer(mDrawerList);
+        syncSlideMenu();
+        updateHeader(fragment);
+    }
+    
+    private Fragment getCurrentFragment() {
+        return getSupportFragmentManager().findFragmentByTag(FRAGMENT_STACK_TAG);
+    }
+    
+    private void handleLastBackPressed() {
+        trackDialog(UIUtils.showYesNoDialog(this, R.string.are_you_sure_you_wanna_leave, R.string.minimize_frostwire, new OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        }));
+    }
+    
+    private void syncSlideMenu() {
+//        if (menuFragment != null) {
+//            Fragment fragment = getCurrentFragment();
+//
+//            if (fragment instanceof SearchFragment) {
+//                menuFragment.setSelectedItem(R.id.menu_main_search);
+//            } else if (fragment instanceof BrowsePeerFragment) {
+//                menuFragment.setSelectedItem(R.id.menu_main_library);
+//            } else if (fragment instanceof TransfersFragment) {
+//                menuFragment.setSelectedItem(R.id.menu_main_transfers);
+//            } else if (fragment instanceof BrowsePeersFragment ||
+//                       fragment instanceof BrowsePeersDisabledFragment) {
+//                menuFragment.setSelectedItem(R.id.menu_main_peers);
+//            } else if (fragment instanceof AboutFragment) {
+//                menuFragment.setSelectedItem(R.id.menu_main_about);
+//            }
+//        }
     }
 
     private void selectItem(int position) {
