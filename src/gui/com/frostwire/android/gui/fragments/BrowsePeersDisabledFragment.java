96b02296d90b79c36cf0009512a151c963ea1477
Alden Torres <aldenml@gmail.com>
2013-10-13 13:52:02 -0400

    Cleanup.

--- src/gui/com/frostwire/android/gui/fragments/BrowsePeersDisabledFragment.java
+++ src/gui/com/frostwire/android/gui/fragments/BrowsePeersDisabledFragment.java
@@ -1,6 +1,6 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011, 2012, FrostWire(TM). All rights reserved.
+ * Copyright (c) 2011-2013, FrostWire(R). All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
 package com.frostwire.android.gui.fragments;
 
 import android.app.Activity;
-import android.support.v4.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
