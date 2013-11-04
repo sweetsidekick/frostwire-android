b7226b65f467a980b3636e162277143558142576
gubatron <gubatron@gmail.com>
2013-11-04 16:01:41 -0500

    changelog

--- src/core/com/frostwire/android/core/Constants.java
+++ src/core/com/frostwire/android/core/Constants.java
@@ -40,7 +40,7 @@ public final class Constants {
      * needing to pass the Android app context to obtain the PackageManager instance.  */
     public static final String FROSTWIRE_BUILD = "115";
 
-    public static final boolean IS_AMAZON_DISTRIBUTION = false;
+    public static final boolean IS_AMAZON_DISTRIBUTION = true;
 
     /**
      * 65280 - Default LAN bound port
