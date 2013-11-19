a82771f0186db00863bd602eb5d53ba700cc722e
Alden Torres <aldenml@gmail.com>
2013-11-19 11:51:28 -0500

    First porting of new cloud downloader.

---
+++ src/gui/com/frostwire/android/gui/transfers/YouTubeDownload2.java
@@ -0,0 +1,493 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011, 2012, FrostWire(TM). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.frostwire.android.gui.transfers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+
+import android.util.Log;
+
+import com.frostwire.android.R;
+import com.frostwire.android.gui.Librarian;
+import com.frostwire.android.gui.services.Engine;
+import com.frostwire.android.gui.util.SystemUtils;
+import com.frostwire.search.extractors.YouTubeExtractor.LinkInfo;
+import com.frostwire.search.youtube.YouTubeCrawledSearchResult;
+import com.frostwire.util.HttpClient;
+import com.frostwire.util.HttpClient.HttpClientListener;
+import com.frostwire.util.HttpClientFactory;
+import com.frostwire.util.HttpClientType;
+import com.frostwire.util.MP4Muxer;
+import com.frostwire.util.MP4Muxer.MP4Metadata;
+
+/**
+ * @author gubatron
+ * @author aldenml
+ *
+ */
+public final class YouTubeDownload2 implements DownloadTransfer {
+
+    private static final String TAG = "FW.HttpDownload";
+
+    static final int STATUS_DOWNLOADING = 1;
+    static final int STATUS_COMPLETE = 2;
+    static final int STATUS_ERROR = 3;
+    static final int STATUS_CANCELLED = 4;
+    static final int STATUS_WAITING = 5;
+    static final int STATUS_UNCOMPRESSING = 6;
+
+    private static final int SPEED_AVERAGE_CALCULATION_INTERVAL_MILLISECONDS = 1000;
+
+    private final TransferManager manager;
+    private final YouTubeCrawledSearchResult sr;
+    private final DownloadType downloadType;
+
+    private final File completeFile;
+    private final File tempVideo;
+    private final File tempAudio;
+
+    private final HttpClient httpClient;
+    private final HttpClientListener httpClientListener;
+    private final Date dateCreated;
+
+    private final long size;
+    private int status;
+    private long bytesReceived;
+    public long averageSpeed; // in bytes
+
+    // variables to keep the download rate of file transfer
+    private long speedMarkTimestamp;
+    private long totalReceivedSinceLastSpeedStamp;
+
+    YouTubeDownload2(TransferManager manager, YouTubeCrawledSearchResult sr) {
+        this.manager = manager;
+        this.sr = sr;
+        this.downloadType = buildDownloadType(sr);
+        this.size = sr.getSize();
+
+        String filename = sr.getFilename();
+
+        completeFile = buildFile(SystemUtils.getTorrentDataDirectory(), filename);
+        tempVideo = buildTempFile(FilenameUtils.getBaseName(filename), "video");
+        tempAudio = buildTempFile(FilenameUtils.getBaseName(filename), "audio");
+
+        bytesReceived = 0;
+        dateCreated = new Date();
+
+        httpClientListener = new HttpDownloadListenerImpl();
+
+        httpClient = HttpClientFactory.newInstance(HttpClientType.PureJava);
+        httpClient.setListener(httpClientListener);
+
+        this.status = STATUS_DOWNLOADING;
+    }
+
+    private static File buildFile(File savePath, String name) {
+        String baseName = FilenameUtils.getBaseName(name);
+        String ext = FilenameUtils.getExtension(name);
+
+        File f = new File(savePath, name);
+        int i = 1;
+        while (f.exists() && i < Integer.MAX_VALUE) {
+            f = new File(savePath, baseName + " (" + i + ")." + ext);
+            i++;
+        }
+        return f;
+    }
+
+    private static File buildTempFile(String name, String ext) {
+        return new File(SystemUtils.getTempDirectory(), name + "." + ext);
+    }
+
+    private DownloadType buildDownloadType(YouTubeCrawledSearchResult sr) {
+        DownloadType dt;
+
+        if (sr.getVideo() != null && sr.getAudio() == null) {
+            dt = DownloadType.VIDEO;
+        } else if (sr.getVideo() != null && sr.getAudio() != null) {
+            dt = DownloadType.DASH;
+        } else if (sr.getVideo() == null && sr.getAudio() != null) {
+            dt = DownloadType.DEMUX;
+        } else {
+            throw new IllegalArgumentException("Not track specified");
+        }
+
+        return dt;
+    }
+
+    public String getDisplayName() {
+        return sr.getDisplayName();
+    }
+
+    public String getStatus() {
+        return getStatusString(status);
+    }
+
+    public int getProgress() {
+        if (size > 0) {
+            return isComplete() ? 100 : (int) ((bytesReceived * 100) / size);
+        } else {
+            return 0;
+        }
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public long getBytesReceived() {
+        return bytesReceived;
+    }
+
+    public long getBytesSent() {
+        return 0;
+    }
+
+    public long getDownloadSpeed() {
+        return (!isDownloading()) ? 0 : averageSpeed;
+    }
+
+    public long getUploadSpeed() {
+        return 0;
+    }
+
+    public long getETA() {
+        if (size > 0) {
+            long speed = getDownloadSpeed();
+            return speed > 0 ? (size - getBytesReceived()) / speed : Long.MAX_VALUE;
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean isComplete() {
+        if (bytesReceived > 0) {
+            return bytesReceived == size && status == STATUS_COMPLETE;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isDownloading() {
+        return status == STATUS_DOWNLOADING;
+    }
+
+    public List<? extends TransferItem> getItems() {
+        return Collections.emptyList();
+    }
+
+    public File getSavePath() {
+        return completeFile;
+    }
+
+    public void cancel() {
+        cancel(false);
+    }
+
+    public void cancel(boolean deleteData) {
+        if (status != STATUS_COMPLETE) {
+            status = STATUS_CANCELLED;
+        }
+        if (status != STATUS_COMPLETE || deleteData) {
+            cleanup();
+        }
+        manager.remove(this);
+    }
+
+    public void start() {
+        if (downloadType == DownloadType.DEMUX) {
+            start(sr.getAudio(), tempAudio);
+        } else {
+            start(sr.getVideo(), tempVideo);
+        }
+    }
+
+    int getStatusCode() {
+        return status;
+    }
+
+    private void start(final LinkInfo inf, final File temp) {
+        status = STATUS_WAITING;
+
+        Engine.instance().getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    status = STATUS_DOWNLOADING;
+                    httpClient.save(inf.link, temp, false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    httpClientListener.onError(httpClient, e);
+                }
+            }
+        });
+    }
+
+    private String getStatusString(int status) {
+        int resId;
+        switch (status) {
+        case STATUS_DOWNLOADING:
+            resId = R.string.peer_http_download_status_downloading;
+            break;
+        case STATUS_COMPLETE:
+            resId = R.string.peer_http_download_status_complete;
+            break;
+        case STATUS_ERROR:
+            resId = R.string.peer_http_download_status_error;
+            break;
+        case STATUS_CANCELLED:
+            resId = R.string.peer_http_download_status_cancelled;
+            break;
+        case STATUS_WAITING:
+            resId = R.string.peer_http_download_status_waiting;
+            break;
+        case STATUS_UNCOMPRESSING:
+            resId = R.string.http_download_status_uncompressing;
+            break;
+        default:
+            resId = R.string.peer_http_download_status_unknown;
+            break;
+        }
+        return String.valueOf(resId);
+    }
+
+    private void updateAverageDownloadSpeed() {
+        long now = System.currentTimeMillis();
+
+        if (isComplete()) {
+            averageSpeed = 0;
+            speedMarkTimestamp = now;
+            totalReceivedSinceLastSpeedStamp = 0;
+        } else if (now - speedMarkTimestamp > SPEED_AVERAGE_CALCULATION_INTERVAL_MILLISECONDS) {
+            averageSpeed = ((bytesReceived - totalReceivedSinceLastSpeedStamp) * 1000) / (now - speedMarkTimestamp);
+            speedMarkTimestamp = now;
+            totalReceivedSinceLastSpeedStamp = bytesReceived;
+        }
+    }
+
+    private void complete() {
+        boolean success = true;
+
+        if (success) {
+            status = STATUS_COMPLETE;
+
+            manager.incrementDownloadsToReview();
+            Engine.instance().notifyDownloadFinished(getDisplayName(), getSavePath());
+
+            if (completeFile.getAbsoluteFile().exists()) {
+                Librarian.instance().scan(getSavePath().getAbsoluteFile());
+            }
+
+            cleanupIncomplete();
+        } else {
+            error(new Exception("Error"));
+        }
+    }
+
+    private void error(Throwable e) {
+        if (status != STATUS_CANCELLED) {
+            if (e != null) {
+                Log.e(TAG, String.format("Error downloading url: %s", sr.getDownloadUrl()), e);
+            } else {
+                Log.e(TAG, String.format("Error downloading url: %s", sr.getDownloadUrl()));
+            }
+            status = STATUS_ERROR;
+            cleanup();
+        }
+    }
+
+    private void cleanup() {
+        try {
+            cleanupComplete();
+            cleanupIncomplete();
+        } catch (Throwable tr) {
+            // ignore
+        }
+    }
+
+    static void simpleHTTP(String url, OutputStream out) throws Throwable {
+        simpleHTTP(url, out, 1000);
+    }
+
+    static void simpleHTTP(String url, OutputStream out, int timeout) throws Throwable {
+        URL u = new URL(url);
+        URLConnection con = u.openConnection();
+        con.setConnectTimeout(timeout);
+        con.setReadTimeout(timeout);
+        InputStream in = con.getInputStream();
+        try {
+
+            byte[] b = new byte[1024];
+            int n = 0;
+            while ((n = in.read(b, 0, b.length)) != -1) {
+                out.write(b, 0, n);
+            }
+        } finally {
+            try {
+                out.close();
+            } catch (Throwable e) {
+                // ignore   
+            }
+            try {
+                in.close();
+            } catch (Throwable e) {
+                // ignore   
+            }
+        }
+    }
+
+    @Override
+    public String getDetailsUrl() {
+        return sr.getDetailsUrl();
+    }
+
+    private static enum DownloadType {
+        VIDEO, DASH, DEMUX
+    }
+
+    private final class HttpDownloadListenerImpl implements HttpClientListener {
+        @Override
+        public void onError(HttpClient client, Exception e) {
+            error(e);
+        }
+
+        @Override
+        public void onData(HttpClient client, byte[] buffer, int offset, int length) {
+            if (status != STATUS_COMPLETE && status != STATUS_CANCELLED) {
+                bytesReceived += length;
+                updateAverageDownloadSpeed();
+                status = STATUS_DOWNLOADING;
+            }
+        }
+
+        @Override
+        public void onComplete(HttpClient client) {
+            if (downloadType == DownloadType.VIDEO) {
+                boolean renameTo = tempVideo.renameTo(completeFile);
+
+                if (!renameTo) {
+                    error(null);
+                } else {
+                    complete();
+                }
+            } else if (downloadType == DownloadType.DEMUX) {
+                try {
+                    new MP4Muxer().demuxAudio(tempAudio.getAbsolutePath(), completeFile.getAbsolutePath(), buildMetadata());
+
+                    if (!completeFile.exists()) {
+                        error(null);
+                    } else {
+                        complete();
+                    }
+
+                } catch (Exception e) {
+                    error(e);
+                }
+            } else if (downloadType == DownloadType.DASH) {
+                if (tempVideo.exists() && !tempAudio.exists()) {
+                    start(sr.getAudio(), tempAudio);
+                } else if (tempVideo.exists() && tempAudio.exists()) {
+                    try {
+                        new MP4Muxer().mux(tempVideo.getAbsolutePath(), tempAudio.getAbsolutePath(), completeFile.getAbsolutePath(), buildMetadata());
+
+                        if (!completeFile.exists()) {
+                            error(null);
+                        } else {
+                            complete();
+                        }
+
+                    } catch (Exception e) {
+                        error(e);
+                    }
+                } else {
+                    error(null);
+                }
+            } else {
+                // warning!!! if this point is reached review the logic
+                error(null);
+            }
+        }
+
+        @Override
+        public void onCancel(HttpClient client) {
+            cleanup();
+            status = STATUS_CANCELLED;
+        }
+
+        @Override
+        public void onHeaders(HttpClient httpClient, Map<String, List<String>> headerFields) {
+        }
+    }
+
+    private void cleanupIncomplete() {
+        cleanupFile(tempVideo);
+        cleanupFile(tempAudio);
+    }
+
+    private void cleanupComplete() {
+        cleanupFile(completeFile);
+    }
+
+    private void cleanupFile(File f) {
+        if (f.exists()) {
+            boolean delete = f.delete();
+            if (!delete) {
+                f.deleteOnExit();
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof YouTubeDownload2)) {
+            return false;
+        }
+
+        return sr.getDownloadUrl().equals(((YouTubeDownload2) obj).sr.getDownloadUrl());
+    }
+
+    private MP4Metadata buildMetadata() {
+        String title = sr.getDisplayName();
+        String author = sr.getDetailsUrl();
+        String source = "YouTube.com";
+
+        String jpgUrl = sr.getVideo() != null ? sr.getVideo().thumbnails.normal : null;
+        if (jpgUrl == null && sr.getAudio() != null) {
+            jpgUrl = sr.getAudio() != null ? sr.getAudio().thumbnails.normal : null;
+        }
+
+        byte[] jpg = jpgUrl != null ? HttpClientFactory.newDefaultInstance().getBytes(jpgUrl) : null;
+
+        return new MP4Metadata(title, author, source, jpg);
+    }
+}
