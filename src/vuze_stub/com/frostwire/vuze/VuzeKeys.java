5cb60d847c4c4519a5860f6987871a07dd7f33a7
Alden Torres <aldenml@gmail.com>
2014-03-16 13:14:44 -0400

    Added vuze_stub for future ui development.

---
+++ src/vuze_stub/com/frostwire/vuze/VuzeKeys.java
@@ -0,0 +1,11 @@
+package com.frostwire.vuze;
+
+public interface VuzeKeys {
+
+    public static final String MAX_DOWNLOAD_SPEED = "Max Download Speed KBs";
+    public static final String MAX_UPLOAD_SPEED = "Max Upload Speed KBs";
+    public static final String MAX_DOWNLOADS = "max downloads";
+    public static final String MAX_UPLOADS = "Max Uploads";
+    public static final String MAX_TOTAL_CONNECTIONS = "Max.Peer.Connections.Total";
+    public static final String MAX_TORRENT_CONNECTIONS = "Max.Peer.Connections.Per.Torrent";
+}
---
+++ src/vuze_stub/com/frostwire/vuze/VuzeUtils.java
@@ -0,0 +1,40 @@
+package com.frostwire.vuze;
+
+import java.io.File;
+import java.util.Set;
+
+import com.frostwire.vuze.VuzeUtils.InfoSetQuery;
+
+public class VuzeUtils {
+
+    public enum InfoSetQuery {
+        NO_SKIPPED, SKIPPED
+
+    }
+
+    public static Set<File> getIgnorableFiles() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void removeDownload(VuzeDownloadManager downloadManager, boolean deleteData, boolean deleteData2) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public static Set<VuzeFileInfo> getFileInfoSet(VuzeDownloadManager downloadManager, InfoSetQuery noSkipped) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void remove(VuzeDownloadManager dm, boolean b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public static void remove(byte[] hash, boolean b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
