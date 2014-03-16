c1f5e4ea5a0a8440ce34751f3e4b33484ccc19f1
Alden Torres <aldenml@gmail.com>
2014-03-16 12:35:52 -0400

    Fixed minir appia realted compilation issue (to fix when merge with appia branch).

--- src/gui/com/frostwire/android/gui/SearchEngine.java
+++ src/gui/com/frostwire/android/gui/SearchEngine.java
@@ -25,7 +25,7 @@ import com.frostwire.android.core.ConfigurationManager;
 import com.frostwire.android.core.Constants;
 import com.frostwire.android.gui.util.OSUtils;
 import com.frostwire.search.SearchPerformer;
-import com.frostwire.search.appia.AppiaSearchPerformer;
+//import com.frostwire.search.appia.AppiaSearchPerformer;
 import com.frostwire.search.archiveorg.ArchiveorgSearchPerformer;
 import com.frostwire.search.bitsnoop.BitSnoopSearchPerformer;
 import com.frostwire.search.domainalias.DomainAliasManager;
@@ -166,12 +166,12 @@ public abstract class SearchEngine {
         }
     };
     
-    public static final SearchEngine APPIA = new SearchEngine("Appia", Constants.PREF_KEY_SEARCH_USE_APPIA) {
-        @Override
-        public SearchPerformer getPerformer(long token, String keywords) {
-            return new AppiaSearchPerformer(new DomainAliasManager("appia.frostclick.com"), token, keywords, DEFAULT_TIMEOUT, FROSTWIRE_ANDROID_USER_AGENT);
-        }
-    };
+//    public static final SearchEngine APPIA = new SearchEngine("Appia", Constants.PREF_KEY_SEARCH_USE_APPIA) {
+//        @Override
+//        public SearchPerformer getPerformer(long token, String keywords) {
+//            return new AppiaSearchPerformer(new DomainAliasManager("appia.frostclick.com"), token, keywords, DEFAULT_TIMEOUT, FROSTWIRE_ANDROID_USER_AGENT);
+//        }
+//    };
     
-    private static final List<SearchEngine> ALL_ENGINES = Arrays.asList(YOUTUBE, FROSTCLICK, MININOVA, BITSNOOP, EXTRATORRENT, SOUNCLOUD, ARCHIVE, TORLOCK, EZTV, APPIA);
+    private static final List<SearchEngine> ALL_ENGINES = Arrays.asList(YOUTUBE, FROSTCLICK, MININOVA, BITSNOOP, EXTRATORRENT, SOUNCLOUD, ARCHIVE, TORLOCK, EZTV);//, APPIA);
 }
