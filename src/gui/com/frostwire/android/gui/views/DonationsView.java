d91fe6b2e97c4ca5b91a09e0df7207dc1485d338
Alden Torres <aldenml@gmail.com>
2014-04-16 12:06:15 -0400

    Minor cleanup.

--- src/gui/com/frostwire/android/gui/views/DonationsView.java
+++ src/gui/com/frostwire/android/gui/views/DonationsView.java
@@ -25,7 +25,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.frostwire.android.R;
 import com.frostwire.android.gui.billing.Biller;
