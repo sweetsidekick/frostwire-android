795c7cd2ea44841b16f5a0ff3e27441250ba6af7
Alden Torres <aldenml@gmail.com>
2014-05-14 09:26:08 -0400

    Setup BTC button in new donations view.

--- src/gui/com/frostwire/android/gui/views/DonationsView2.java
+++ src/gui/com/frostwire/android/gui/views/DonationsView2.java
@@ -21,9 +21,11 @@ package com.frostwire.android.gui.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.frostwire.android.R;
+import com.frostwire.android.gui.util.OfferUtils;
 
 /**
  * @author guabtron
@@ -36,5 +38,20 @@ public class DonationsView2 extends LinearLayout {
         super(context, attrs);
 
         View.inflate(context, R.layout.view_donations2, this);
+
+        Button btc = (Button) findViewById(R.id.view_donations_button_bitcoin);
+        btc.setOnClickListener(new BitcoinDonationListener(context));
+    }
+
+    private static final class BitcoinDonationListener extends ClickAdapter2<Context> {
+
+        public BitcoinDonationListener(Context ctx) {
+            super(ctx);
+        }
+
+        @Override
+        public void onClick(Context ctx, View v) {
+            OfferUtils.onBTCDonationButtonClick(ctx);
+        }
     }
 }
