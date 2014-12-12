986c31c8bdba0e42bff9414067268dd816dba611
Alden Torres <aldenml@gmail.com>
2014-12-12 09:07:03 -0500

    Fixed error reported in console. Cleanup.

--- apollo/src/com/andrew/apollo/utils/MusicUtils.java
+++ apollo/src/com/andrew/apollo/utils/MusicUtils.java
@@ -946,20 +946,25 @@ public final class MusicUtils {
         if (id == -1) {
             return null;
         }
-        Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, id);
-        Cursor cursor = context.getContentResolver().query(uri, new String[] {
+        try {
+            Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, id);
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{
                     AlbumColumns.NUMBER_OF_SONGS
-                }, null, null, null);
-        String songCount = null;
-        if (cursor != null) {
-            cursor.moveToFirst();
-            if (!cursor.isAfterLast()) {
-                songCount = cursor.getString(0);
+            }, null, null, null);
+            String songCount = null;
+            if (cursor != null) {
+                cursor.moveToFirst();
+                if (!cursor.isAfterLast()) {
+                    songCount = cursor.getString(0);
+                }
+                cursor.close();
+                cursor = null;
             }
-            cursor.close();
-            cursor = null;
+            return songCount;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return null;
         }
-        return songCount;
     }
 
     /**
