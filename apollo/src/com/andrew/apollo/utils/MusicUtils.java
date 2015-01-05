c1b4e30a77a899a29a79eff0e1bf4341a2214ee8
Alden Torres <aldenml@gmail.com>
2015-01-05 13:23:25 -0500

    Fixed SQL error reported in Play console.

--- apollo/src/com/andrew/apollo/utils/MusicUtils.java
+++ apollo/src/com/andrew/apollo/utils/MusicUtils.java
@@ -1036,20 +1036,25 @@ public final class MusicUtils {
         if (id == -1) {
             return null;
         }
-        Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, id);
-        Cursor cursor = context.getContentResolver().query(uri, new String[] {
+        try {
+            Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, id);
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{
                     AlbumColumns.FIRST_YEAR
-                }, null, null, null);
-        String releaseDate = null;
-        if (cursor != null) {
-            cursor.moveToFirst();
-            if (!cursor.isAfterLast()) {
-                releaseDate = cursor.getString(0);
+            }, null, null, null);
+            String releaseDate = null;
+            if (cursor != null) {
+                cursor.moveToFirst();
+                if (!cursor.isAfterLast()) {
+                    releaseDate = cursor.getString(0);
+                }
+                cursor.close();
+                cursor = null;
             }
-            cursor.close();
-            cursor = null;
+            return releaseDate;
+        } catch (Throwable e) {
+            // ignore this error since it's not critical
+            return null;
         }
-        return releaseDate;
     }
 
     /**
