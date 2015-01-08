0bc8deaff4c9b5c7c3e082f46fc9fadc7dc7fd3f
gubatron <gubatron@gmail.com>
2015-01-08 15:46:36 -0500

    NPE on createPlaylist

--- apollo/src/com/andrew/apollo/utils/MusicUtils.java
+++ apollo/src/com/andrew/apollo/utils/MusicUtils.java
@@ -862,6 +862,7 @@ public final class MusicUtils {
      * @return A new playlist ID.
      */
     public static final long createPlaylist(final Context context, final String name) {
+        long result = -1;
         if (name != null && name.length() > 0) {
             final ContentResolver resolver = context.getContentResolver();
             final String[] projection = new String[] {
@@ -870,20 +871,20 @@ public final class MusicUtils {
             final String selection = PlaylistsColumns.NAME + " = '" + name + "'";
             Cursor cursor = resolver.query(MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
                     projection, selection, null, null);
-            if (cursor.getCount() <= 0) {
+            if (cursor != null && cursor.getCount() <= 0) {
                 final ContentValues values = new ContentValues(1);
                 values.put(PlaylistsColumns.NAME, name);
                 final Uri uri = resolver.insert(MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
                         values);
-                return Long.parseLong(uri.getLastPathSegment());
+                result = Long.parseLong(uri.getLastPathSegment());
             }
+
             if (cursor != null) {
                 cursor.close();
                 cursor = null;
             }
-            return -1;
         }
-        return -1;
+        return result;
     }
 
     /**
